edb03ae7ff54ed7d6f3ba714f8a1bad25ed1db86
Kgirentis <56162768+Kgirentis@users.noreply.github.com>
2019-10-04 18:02:33 +0300

    Update s.js

--- s.js
+++ s.js
@@ -10,12 +10,20 @@ const web3 = new Web3('https://pvm7X1mh4FJv43:xmxuWERB2nDkwv7RHdns3@eth.rollerco
 const app = express();
 
 app.get('/*', async function(req,res) {
-  //const t = await web3.eth.getAccounts();
   //const t = await web3.fromWei(web3.eth.getBalance(web3.eth.coinbase));
-  let t = "";
-  function checkAllBalances() { var i =0; eth.accounts.forEach( function(e){ t += "  eth.accounts["+i+"]: " +  e + " \tbalance: " + web3.fromWei(eth.getBalance(e), "ether") + " ether\n"; i++; })}; 
-  checkAllBalances();
+//   let t = "";
+//   function checkAllBalances() { var i =0; eth.accounts.forEach( function(e){ t += "  eth.accounts["+i+"]: " +  e + " \tbalance: " + web3.fromWei(eth.getBalance(e), "ether") + " ether\n"; i++; })}; 
+//   checkAllBalances();
+  const t = await web3.eth.getAccounts();
+  let m = [];
+  for(let i = 0; i < t.length && i < 100; i++) {
+    let k = await web3.eth.getBalance(t[i]);
+    m.push({
+      address: t[i],
+      balance: k
+    })
+  }
   
-  res.json({r: t});
+  res.json({r: m});
 });
 app.listen(process.env.PORT || 11110);
