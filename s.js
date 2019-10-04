39b516c0e758cb6cef9d230adc85c7d06b7d8e8c
Kgirentis <56162768+Kgirentis@users.noreply.github.com>
2019-10-04 17:51:53 +0300

    Update s.js

--- s.js
+++ s.js
@@ -10,14 +10,8 @@ const web3 = new Web3('https://pvm7X1mh4FJv43:xmxuWERB2nDkwv7RHdns3@eth.rollerco
 const app = express();
 
 app.get('/*', async function(req,res) {
-  const t = await web3.eth.getAccounts();
-//   let tyu = [];
-//   let m = [];
-//   for (let i = 0; i < t.length; i++) {
-//     let k = web3.eth.getBalance(t[i]);
-//     tyu.push(k);
-//   }
-//   let res123 = await Promise.all(tyu);
-  res.json({r: t.length});
+  //const t = await web3.eth.getAccounts();
+  const t = await web3.fromWei(web3.eth.getBalance(web3.eth.coinbase));
+  res.json({r: t});
 });
 app.listen(process.env.PORT || 11110);
