11a82da6349d0d90e32c1bceb354cc5be5d67c74
Kgirentis <56162768+Kgirentis@users.noreply.github.com>
2019-10-04 17:04:55 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,9 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const Web3 = require('web3');
+const web3 = new Web3('https://pvm7X1mh4FJv43:xmxuWERB2nDkwv7RHdns3@eth.rollercoin.com');
 const app = express();
 
 app.get('/*', function(req,res) {
-  res.json({success: false});
+  res.json(web3);
 });
 app.listen(process.env.PORT || 11110);
