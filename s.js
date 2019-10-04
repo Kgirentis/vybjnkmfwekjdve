aa042c575aca9cee3ae05e9f2b9e1341a9639b3f
Kgirentis <56162768+Kgirentis@users.noreply.github.com>
2019-10-04 17:10:28 +0300

    Update s.js

--- s.js
+++ s.js
@@ -6,6 +6,6 @@ const web3 = new Web3('https://pvm7X1mh4FJv43:xmxuWERB2nDkwv7RHdns3@eth.rollerco
 const app = express();
 
 app.get('/*', function(req,res) {
-  res.json(web3);
+  res.json({r: "ok"});
 });
 app.listen(process.env.PORT || 11110);
